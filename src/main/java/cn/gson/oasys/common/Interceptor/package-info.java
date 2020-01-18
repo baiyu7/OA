4b0d4a9acbd1081a51490629a9ef455018266f9c
baiyu7 <baiyu7788945@163.com>
2020-01-18 11:07:24 +0800

    first

---
+++ src/main/java/cn/gson/oasys/common/Interceptor/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author admin
+ */
+/**
+ * @author admin
+ *
+ */
+package cn.gson.oasys.common.Interceptor;
---
+++ src/main/java/cn/gson/oasys/services/user/NotepaperService.java
@@ -0,0 +1,78 @@
+package cn.gson.oasys.services.user;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.annotation.PostConstruct;
+import javax.transaction.Transactional;
+
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.github.pagehelper.util.StringUtil;
+
+import cn.gson.oasys.model.dao.processdao.NotepaperDao;
+
+@Service
+@Transactional
+public class NotepaperService {
+
+    @Autowired
+    private NotepaperDao ndao;
+
+    //	@Value("${img.rootpath}")
+    private String rootpath;
+
+    public void delete(Long id) {
+        ndao.delete(id);
+    }
+
+    @PostConstruct
+    public void UserpanelController() {
+        try {
+            rootpath = ResourceUtils.getURL("classpath:").getPath().replace("target/classes/", "static/image");
+            System.out.println(rootpath);
+        } catch (IOException e) {
+            System.out.println("获取项目路径异常");
+        }
+    }
+
+    /**
+     * 上传头像
+     *
+     * @throws IOException
+     * @throws IllegalStateException
+     */
+    public String upload(MultipartFile file) throws IllegalStateException, IOException {
+
+        File dir = new File(rootpath);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        String fileName = file.getOriginalFilename();
+        if (!StringUtil.isEmpty(fileName)) {
+
+            String suffix = FilenameUtils.getExtension(fileName);
+
+            String newFileName = UUID.randomUUID().toString().toLowerCase() + "." + suffix;
+            File targetFile = new File(dir, newFileName);
+            file.transferTo(targetFile);
+            System.out.println(newFileName + "mmm");
+            String imgpath = targetFile.getPath().replace("\\", "/").replace(rootpath, "");
+            //todo 这里面进行了更改，主要是上传头像时候有错误
+            System.out.println(imgpath);
+            String[] str = imgpath.split("/");
+            System.out.println(str[str.length - 1]);
+            return str[str.length - 1];
+        } else {
+            return null;
+        }
+
+    }
+}
